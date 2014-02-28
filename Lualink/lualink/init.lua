614d53b378e4b659aee12adca3cae9b40705f015
Stephen McIntosh <stephenmac7@gmail.com>
2014-02-27 23:02:28 -0500

    Added init

---
+++ Lualink/lualink/init.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.motor = require "lualink.motor"
+M.time = require "lualink.time"
+M.create = require "lualink.create"
+M.sensor = require "lualink.sensor"
+M.servo = require "lualink.servo"
+
+M.PosMotor = require "lualink.PosMotor"
+M.WheelController = require "lualink.WheelController"
+M.Servo = require "lualink.Servo"
+
+return M
