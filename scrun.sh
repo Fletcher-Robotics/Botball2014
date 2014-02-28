638694930d62858233026b2f0d6a3cd50388208d
Stephen McIntosh <stephenmac7@gmail.com>
2014-02-28 13:51:04 -0500

    Added exclude file

--- scrun.sh
+++ scrun.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Copies all files (except those in .exclude) to the local computer from
+# computer at IP
 USER=stephen
 IP=192.168.1.7
 DIR=Development/Botball2014
 
 rsync -P --delete -rl -e ssh --exclude-from=.exclude \
-  $USER@$IP:/home/$USER/$DIR/* .
+  $USER@$IP:/home/$USER/$DIR/. .
 lua $1
