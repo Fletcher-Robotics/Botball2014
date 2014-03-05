874243e2fca79e324cfb6c71d95a0bf680f368d1
Stephen McIntosh <stephenmac7@gmail.com>
2014-03-05 17:01:48 -0500

    Updated hangar.lua

--- hangar.lua
+++ hangar.lua
@@ -1,30 +1,43 @@
 local create = require("lualink.create")
 local manager = require("lualink.managers")
+local msleep = require("lualink.time").msleep
 
 function main()
-	local claw = manager.Servo:new(0, {open = 700, closed = 1450})
-	local arm = manager.PosMotor:new(0, 500, {neutral = 0, max=1400, topbar=1380})
+	local claw = manager.Servo:new(0, {open = 700, closed = 1450, sorta_open = 1000})
+	local arm = manager.PosMotor:new(0, 500, {neutral = 0, max=1400, topbar=1350, botbar=-290})
 	arm:max()
 
+	-- Navigate to hangers
 	create.drive_arc(255, -400, -55)
 	create.drive_arc(255, 520, 51)
 	create.drive_segment(150, 220)
-	create.spin_angle(150, -87)
-	create.drive_segment(150, 225)
+	create.spin_angle(150, -88.8)
+	create.drive_segment(150, 240)
 	create.force_wait()
 
+	-- Hang greens
 	arm:topbar()
 	arm:bmd()
-	claw:open()
-end
+	msleep(750)
+	claw:sorta_open()
 
-function reset_arm(arm)
+	-- Move back
+	create.drive_segment(150, -180)
+	create.drive_arc(150, 200, -45)
+	create.drive_arc(150, -200, 39)
+	msleep(250)
+	arm:botbar()
+	arm:bmd()
+
+	-- Go for blue one
+	create.drive_segment(200, 280)
+	create.force_wait()
+	claw:closed()
 	arm:neutral()
 	arm:bmd()
-	arm:freeze()
+	create.drive_segment(200, -180)
 end
 
 create.connect()
 main()
 create.disconnect()
-collectgarbage()
